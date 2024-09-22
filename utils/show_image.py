07178d151611fb7083b6f4db7e5930068cf17e0c
AveMaria23 <demyn300@gmail.com>
2024-09-22 18:51:07 +0300

    Revert "init"
    This reverts commit febea2f47b646037394aafa6dc36b599f66bcbfc.

--- utils/show_image.py
+++
@@ -1,6 +0,0 @@
-def show_image(plt, image, title, duration=None):
-    if duration is not None:
-        title += ' duration: ' + str(duration)
-    plt.title(title, color='red')
-    plt.imshow(image, cmap='gray')
-    plt.axis('off')
